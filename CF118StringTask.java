94783096fe11402e95c0e21010e2bd0fa8022408
Brian Yeicol Restrepo Tangarife <yeicores72@gmail.com>
2016-05-06 11:04:32 -0500

    create CF118StringTask.java

---
+++ CF118StringTask.java
@@ -0,0 +1,25 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+/**
+ * See <a href="http://codeforces.com/problemset/problem/118/A">String Task</a>
+ *
+ * @author Brian Yeicol Restrepo Tangarife
+ */
+public class CF118StringTask {
+
+	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	static PrintWriter out = new PrintWriter(System.out);
+
+	public static void main(String[] args) throws IOException {
+		String s = in.readLine();
+		s = s.replaceAll("(?i)[AEIYOU]", "");
+		s = s.replaceAll("(?i)([^AEIYOU])", ".$1");
+		s = s.toLowerCase();
+		out.print(s);
+		out.close();
+	}
+}
